c4cc1389aa6af7983027b677bf54bd8569921f52
Simon Abrelat <RespawnAITeam@gmail.com>
2017-12-26 01:28:54 -0500

    sendex FTW

--- MyBot.py
+++ MyBot.py
@@ -1,39 +1,26 @@
 import hlt
 import logging
 from collections import OrderedDict
-game = hlt.Game("Respawn AI")
-logging.info("Respawn Live Long and Prosper")
+game = hlt.Game("SentdeBot-V1")
+logging.info("Starting SentdeBot")
 
 while True:
     game_map = game.update_map()
     command_queue = []
-
+    
     for ship in game_map.get_me().all_ships():
         shipid = ship.id
         if ship.docking_status != ship.DockingStatus.UNDOCKED:
             # Skip this ship
             continue
 
-        def moveFull(target):
-            navigate_command = ship.navigate(
-                ship.closest_point_to(target),
-                game_map,
-                speed=int(hlt.constants.MAX_SPEED),
-                ignore_ships=False)
-            if navigate_command:
-                command_queue.append(navigate_command)
-
         entities_by_distance = game_map.nearby_entities_by_distance(ship)
         entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
-
-        closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if
-                isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not
-                entities_by_distance[distance][0].is_owned()]
+        
+        closest_empty_planets = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
 
         team_ships = game_map.get_me().all_ships()
-        closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if
-                isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not
-                in team_ship]
+        closest_enemy_ships = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Ship) and entities_by_distance[distance][0] not in team_ships]
 
         # If there are any empty planets, let's try to mine!
         if len(closest_empty_planets) > 0:
@@ -41,11 +28,26 @@ while True:
             if ship.can_dock(target_planet):
                 command_queue.append(ship.dock(target_planet))
             else:
-                moveFull(target_planet)
+                navigate_command = ship.navigate(
+                            ship.closest_point_to(target_planet),
+                            game_map,
+                            speed=int(hlt.constants.MAX_SPEED),
+                            ignore_ships=False)
+
+                if navigate_command:
+                    command_queue.append(navigate_command)
+
         # FIND SHIP TO ATTACK!
         elif len(closest_enemy_ships) > 0:
             target_ship = closest_enemy_ships[0]
-            moveFull(target_ship)
+            navigate_command = ship.navigate(
+                        ship.closest_point_to(target_ship),
+                        game_map,
+                        speed=int(hlt.constants.MAX_SPEED),
+                        ignore_ships=False)
+
+            if navigate_command:
+                command_queue.append(navigate_command)
 
     game.send_command_queue(command_queue)
     # TURN END
